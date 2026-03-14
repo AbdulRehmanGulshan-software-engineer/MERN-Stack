80b509083f7bd6057dd8d2757b2a8e145738d40c
Abdul Rehman Gulshan <sabdulrehmangulshan@gmail.com>
2026-03-14 15:34:11 +0500

    learned react + vite setup

---
+++ React/Course_01/Lecture_03/newReact/First Project/src/App.jsx
@@ -0,0 +1,14 @@
+// creating react component,default export
+export default function App() {
+  return(
+    <h1>Hello Big Gamez PUCIT</h1>
+  )
+};
+
+
+// named export
+export function Rohit() {
+  return(
+    <h2>I am Full Stack Developer</h2>
+  )
+};
---
+++ React/Course_01/Lecture_03/newReact/First Project/src/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+import App, { Rohit } from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <>
+    <h1>Hello Coder Army</h1>
+    <App />
+    <Rohit />
+  </>,
+);
