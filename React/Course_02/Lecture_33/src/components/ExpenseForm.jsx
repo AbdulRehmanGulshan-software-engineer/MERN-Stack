ce8084b9325dea8cdec2f4311dfb7c382271053f
Abdul Rehman Gulshan <sabdulrehmangulshan@gmail.com>
2026-04-25 19:50:03 +0500

    studies useRef Hook

--- React/Course_02/Lecture_33/src/components/ExpenseForm.jsx
+++ React/Course_02/Lecture_33/src/components/ExpenseForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "../App.css";
 
 export default function ({ setExpenses }) {
@@ -9,18 +9,31 @@ export default function ({ setExpenses }) {
     amount: "",
   });
 
+  const titleRef = useRef(null);
+  const categoryRef = useRef(null);
+  const amountRef = useRef(null);
+
+  useEffect(() => {
+    console.log(titleRef);
+  });
+
   //submit handler function
   const handleSubmit = (e) => {
     e.preventDefault();
+
     setExpenses((prevState) => [
       ...prevState,
-      { ...expense, id: crypto.randomUUID() },
+      {
+        title: titleRef.current.value,
+        category: categoryRef.current.value,
+        amount: amountRef.current.value,
+      },
     ]);
-    setExpense({
-      title: "",
-      category: "",
-      amount: "",
-    });
+    // setExpense({
+    //   title: "",
+    //   category: "",
+    //   amount: "",
+    // });
   };
 
   return (
@@ -29,29 +42,31 @@ export default function ({ setExpenses }) {
         <div className="input-container">
           <label htmlFor="title">Title</label>
           <input
+            ref={titleRef}
             id="title"
             name="title"
-            value={expense.title}
-            onChange={(e) =>
-              setExpense((prevState) => ({
-                ...prevState,
-                title: e.target.value,
-              }))
-            }
+            // value={expense.title}
+            // onChange={(e) =>
+            //   setExpense((prevState) => ({
+            //     ...prevState,
+            //     title: e.target.value,
+            //   }))
+            // }
           />
         </div>
         <div className="input-container">
           <label htmlFor="category">Category</label>
           <select
+            ref={categoryRef}
             id="category"
             name="category"
-            value={expense.category}
-            onChange={(e) =>
-              setExpense((prevState) => ({
-                ...prevState,
-                category: e.target.value,
-              }))
-            }
+            // value={expense.category}
+            // onChange={(e) =>
+            //   setExpense((prevState) => ({
+            //     ...prevState,
+            //     category: e.target.value,
+            //   }))
+            // }
           >
             <option value="" hidden>
               Select Category
@@ -66,15 +81,16 @@ export default function ({ setExpenses }) {
         <div className="input-container">
           <label htmlFor="amount">Amount</label>
           <input
+            ref={amountRef}
             id="amount"
             name="amount"
-            value={expense.amount}
-            onChange={(e) =>
-              setExpense((prevState) => ({
-                ...prevState,
-                amount: e.target.value,
-              }))
-            }
+            // value={expense.amount}
+            // onChange={(e) =>
+            //   setExpense((prevState) => ({
+            //     ...prevState,
+            //     amount: e.target.value,
+            //   }))
+            // }
           />
         </div>
         <button className="add-btn">Add</button>
