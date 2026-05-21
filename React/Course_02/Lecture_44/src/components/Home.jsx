f0553f616bd5619682ca586f2c3dd603e0fd3d29
Abdul Rehman Gulshan <sabdulrehmangulshan@gmail.com>
2026-05-21 22:02:48 +0500

    added new code

--- React/Course_02/Lecture_44/src/components/Home.jsx
+++ React/Course_02/Lecture_44/src/components/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <div className="text-2xl caret-lime-50">This is home page</div>
-      <NewCounter name="New Counter"></NewCounter>
+      {/* <NewCounter name="New Counter"></NewCounter> */}
       <OldCounter name="Old Counter"/>
     </>
   );
