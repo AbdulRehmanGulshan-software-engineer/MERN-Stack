54ffe5f7cd3444e602881e441a9f8c293a70e2a5
Abdul Rehman Gulshan <sabdulrehmangulshan@gmail.com>
2026-06-10 13:49:57 +0500

    learnings: redux intro and reducer pattern

---
+++ Redux/Lecture_01/script.js
@@ -0,0 +1,94 @@
+// let state = {
+//     count: 0,
+//     counterName: 'Timer',
+//     age: 21
+// }
+
+// function increment() {
+//     // Mutating State 👇
+//     // state.count = state.count + 1
+
+//     const prevState = state;
+//     console.log(prevState)
+//     // Without Mutating 👇
+//     state = {...state, count: state.count + 1 }
+//     console.log(`New State Is : ${state}`)
+//     console.log(prevState==state)
+// }
+
+// increment()
+
+
+
+import { createStore } from "redux"
+
+
+const initialState = {
+    post: 0,
+    name: 'Abdul Rehman Gulshan',
+    age: 26
+}
+
+const INCREMENT = 'post/increment'
+const DECREMENT = 'post/decrement'
+const INCREASE_BY = 'post/incrementBy'
+const DECREASE_BY = 'post/decrementBy'
+
+// redux restrict us to make reducer function
+function reducer(state = initialState, action) {
+    // if (action.type === INCREASE_BY) {
+    //     return { ...state, post: state.post + action.payload }
+    // }
+    // else if (action.type === INCREMENT) {
+    //     return { ...state, post: state.post + 1 }
+    // }
+    // else if (action.type === DECREMENT) {
+    //     return { ...state, post: state.post - 1 }
+    // }
+    // else if (action.type === DECREASE_BY) {
+    //     return { ...state, post: state.post - action.payload }
+    // }
+
+    // Using Switch Statement 👇
+    switch (action.type) {
+        case INCREMENT:
+            return { ...state, post: state.post + 1 }
+        case DECREMENT:
+            return { ...state, post: state.post - 1 }
+        case INCREASE_BY:
+            return { ...state, post: state.post + action.payload }
+        case DECREASE_BY:
+            return { ...state, post: state.post - action.payload }
+        default:
+            return state
+    }
+
+
+    // state = { ...state, post: state.post + 1 }   ⚠️
+    // redux says you don't need to touch state.i will update it.redux will give state to reducer function 
+    //we just have to return state 👇
+
+    return state;
+}
+
+//what redux will do
+// reduxState = reducer(reduxState, { type: 'post/increment' })
+// console.log(reduxState)
+// reduxState = reducer(reduxState, { type: 'post/increment' })
+// console.log(reduxState)
+// reduxState = reducer(reduxState, { type: 'post/decrement' })
+// console.log(reduxState)
+// reduxState = reducer(reduxState, { type: 'post/incrementBy', payload: 2 })
+// console.log(reduxState)
+
+const store = createStore(reducer)
+
+console.log(store)
+
+store.subscribe(() => {
+    console.log(store.getState())
+})
+
+store.dispatch({ type: INCREMENT })
+store.dispatch({ type: INCREMENT })
+store.dispatch({ type: INCREASE_BY, payload: 20 })
