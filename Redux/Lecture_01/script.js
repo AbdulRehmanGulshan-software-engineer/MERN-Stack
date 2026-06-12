c276489faaf5829a87da25dcdbb0d1e6f9d835ed
Abdul Rehman Gulshan <sabdulrehmangulshan@gmail.com>
2026-06-12 16:50:30 +0500

    learnings: custom combineReducers function

--- Redux/Lecture_01/script.js
+++ Redux/Lecture_01/script.js
@@ -1,8 +1,34 @@
-import { combineReducers, createStore } from "redux"
+import { createStore } from "redux"
+// import { combineReducers } from "redux"
 import cartReducer, { CART_ADD_ITEM, CART_ITEM_DECREASE_QUANTITY, CART_ITEM_INCREASE_QUANTITY, CART_REMOVE_ITEM } from "./cartReducer"
 import wishListReducer, { WISHLIST_ADD_ITEM, WISHLIST_REMOVE_ITEM } from "./wishListReducer"
 import productReducer from "./productsReducer"
 // import { productsList } fromed/productsList"
+
+
+//custom combinedReducer
+function combineReducers(reducers) {
+    const reducerKeys = Object.keys(reducers)
+
+    return function (state = {}, action) {
+        const nextstate = {}
+
+        for (let i = 0; i < reducerKeys.length; i++) {
+            const key = reducerKeys[i];
+            const reducer = reducers[key];
+
+            const previousStateForKey = state[key];
+            const nextStateForKey = reducer(previousStateForKey, action)
+            // // instead use this
+            // nextstate[key] = reducer(state[key], action);
+
+            nextstate[key] = nextStateForKey;
+        }
+        return nextstate
+
+    }
+}
+
 
 //Combined All Reducers
 const reducer = combineReducers({
