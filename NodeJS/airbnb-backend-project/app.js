2ca21cdf2b714ab55e8417f3c55af7ee42d593c9
Abdul Rehman Gulshan <sabdulrehmangulshan@gmail.com>
2026-07-14 05:59:00 +0500

    learnings: adding 404 response

--- NodeJS/airbnb-backend-project/app.js
+++ NodeJS/airbnb-backend-project/app.js
@@ -11,6 +11,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(userRouter);
 app.use(hostRouter);
 
+//Adding 404. sab uper walo ko chance mil gya aor ab is pe aa jae ga
+app.use((req, res, next) => {
+    res.status(404).send("<h1>404 Your page is not found on airbnb</h1>")
+})
+
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running at ${PORT}`)
