3e5bc2b3c10110ecc6a42a132a6c4cc1fb59f84d
Abdul Rehman Gulshan <sabdulrehmangulshan@gmail.com>
2026-07-15 13:16:26 +0500

    learnings: MVC NodeJS

---
+++ NodeJS/Lecture-13 MVC/Project - MVC/routes/hostRouter.js
@@ -0,0 +1,11 @@
+// External Module
+const express = require('express');
+const hostRouter = express.Router();
+
+// Local Module
+const { getAddHome, postAddHome } = require('../controllers/homes');
+
+hostRouter.get("/add-home",getAddHome)
+hostRouter.post("/add-home", postAddHome)
+
+exports.hostRouter = hostRouter;
