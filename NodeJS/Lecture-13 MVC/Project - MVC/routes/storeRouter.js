488c2760e65dba09de2e06f335d742f4b1b40d74
Abdul Rehman Gulshan <sabdulrehmangulshan@gmail.com>
2026-07-15 16:38:58 +0500

    learnings: airbnb proj lecture 13

---
+++ NodeJS/Lecture-13 MVC/Project - MVC/routes/storeRouter.js
@@ -0,0 +1,17 @@
+// Core Modules
+const path = require('path');
+
+// External Module
+const express = require('express');
+const storeRouter = express.Router();
+
+// Local Module
+const { getHomes, getBookings, getIndex, getFavouriteList } = require('../controllers/storeController');
+
+storeRouter.get("/", getIndex);
+storeRouter.get("/homes", getHomes);
+storeRouter.get("/bookings", getBookings);
+storeRouter.get("/favourites", getFavouriteList);
+
+module.exports = storeRouter;
+
